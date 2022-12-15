68400632a6052a6dfff0fdcea5d6add47c7650f6
bhumikapatel251 <102209491+bhumikapatel251@users.noreply.github.com>
2022-12-15 15:17:13 +0530

    Initial Commit

---
+++ SwiftValidationForm/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftValidationForm
+//
+//  Created by Bhumika Patel on 15/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
